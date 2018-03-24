50fe05844214ec344eb140afc318696d2d7966ec
DaytonSteffeny <35751232+DaytonSteffeny@users.noreply.github.com>
2018-03-23 20:37:01 -0600

    First commit

---
+++ Color.swift
@@ -0,0 +1,16 @@
+//
+//  Color.swift
+//  Colors
+//
+//  Created by Dayton Steffeny on 3/16/18.
+//  Copyright © 2018 Dayton Steffeny. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+}
+
+
